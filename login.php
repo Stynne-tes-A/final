b757357e721675b6a77589413ce03a04d7304f69
Stynne-tes-A <stynne.mernstack@gmail.com>
2024-11-14 12:32:01 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Momento</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <section class="pageContainer">
+        <main class="row">
+            <div class="col-1">
+                <div class="heroimg"></div>
+            </div>
+            <article class="col-2">
+                <form action=""></form>
+            </article>
+
+
+        </main>
+    </section>
+    
+</body>
+</html>
